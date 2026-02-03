fd85fd0e43a695994ed95b49cbc07f5e9d4aaea4
maulikpatel-SDET <maulik.patel@calidadinfotech.com>
2026-02-03 18:12:14 +0530

    Update catalog_price_check.py

--- catalog_price_check.py
+++ catalog_price_check.py
@@ -133,6 +133,7 @@
 import requests
 from datetime import datetime
 import pytz
+import os
 import concurrent.futures
 
 # SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
